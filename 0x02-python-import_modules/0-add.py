052c5554c77fac26ff126aa31a4d11e37b6fb28d
Caroline <carolinekimatu6@gmail.com>
2022-12-01 03:32:43 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import add_0 as addition
-a = 1
-b = 2
-result = addition.add(a, b)
-print(f"{a:d} + {b:d} = {result:d}")
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    result = addition.add(a, b)
+
+    print(f"{a:d} + {b:d} = {result:d}")
