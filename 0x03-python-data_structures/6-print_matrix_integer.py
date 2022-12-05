78188483ea40b58c46ab4985f1d9552bc8063c5f
Caroline <carolinekimatu6@gmail.com>
2022-12-05 03:05:47 -0800

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#  function that prints a matrix of integers.
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for val in row:
+            print("{:d}".format(val), end='')
+            if val != row[-1]:
+                print(" ", end='')
+        print("")
