0a4c5040064f96debeb177f78b06afd3ee9c2551
Caroline <carolinekimatu6@gmail.com>
2022-12-05 06:50:42 -0800

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,9 +4,8 @@
 # uf idx is -ve or out of range, returns the same list
 
 def delete_at(my_list=[], idx=0):
-    for i in range(len(my_list)):
-        if idx < 0 or idx >= len(my_list):
-            return my_list
-        else:
-            del my_list[idx]
-            return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
