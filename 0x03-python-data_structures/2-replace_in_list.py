13de7215ccdacde3776f5bf7ae0235d6448de6a4
Caroline <carolinekimatu6@gmail.com>
2022-12-05 00:26:49 -0800

    task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# function replaces an element of a list at a specific position
+# if idx is -ve, function should return the original list
+# If idx is out of range, the function should return the original list
+
+def replace_in_list(my_list, idx, element):
+    n = len(my_list)
+    
+    if idx < 0:
+        return my_list
+    elif idx >= n:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
