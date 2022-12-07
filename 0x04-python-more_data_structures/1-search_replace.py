f51884794763f6b646818f8ccafa81495e60f5ce
Caroline <carolinekimatu6@gmail.com>
2022-12-07 02:52:02 -0800

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#  a function that replaces all occurrences
+# of an element by another in a new list
+# my_list is the initial list
+# search is the element to replace in the list
+# replace is the new element
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
