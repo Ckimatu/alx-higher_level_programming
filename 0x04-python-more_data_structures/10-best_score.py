d27365d10fef55bde752597add4615d85d47b8b3
Caroline <carolinekimatu6@gmail.com>
2022-12-07 07:07:23 -0800

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# returns a key with the biggest integer value.
+# all values are only integers
+# If no score found, return None
+
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    output = list(a_dictionary.keys())[0]
+    max_value = a_dictionary[output]
+    for item, item_value in a_dictionary.items():
+        if item_value > max_value:
+            max_value = item_value
+            output = item
+        return (output)
