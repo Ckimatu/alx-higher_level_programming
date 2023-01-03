b319d16ff198f7c06e4cdd2341ea9f479b759efe
Caroline <carolinekimatu6@gmail.com>
2023-01-03 03:49:16 -0800

    change

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,13 +5,27 @@
 class Rectangle:
     """defines a rectangle"""
     def __init__(self, width=0, height=0):
-        """initialiation of widht and height"""
+        """initialiation of width and height"""
         self.width = width
         self.height = height
 
     @property
+    def height(self):
+        """to retrieve private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """to set width with new value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must b >= 0")
+        self.__height = value
+
+    @property
     def width(self):
-        """to retrieve private instance attribute  width"""
+        """retrieve private instance attribute width"""
         return self.__width
 
     @width.setter
@@ -20,19 +34,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must b >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        """retrieve private instance attribute height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """to set height with new value"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
