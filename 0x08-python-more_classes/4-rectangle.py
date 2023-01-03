a7cec230f28f6b1d479a6031778dfb3c98f4467c
Caroline <carolinekimatu6@gmail.com>
2023-01-03 06:38:37 -0800

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initialiation of width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def area(self):
         """public instance method that  returns the rectangle area"""
