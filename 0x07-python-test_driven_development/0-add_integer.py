e961c8125bf3c577bc2546c37738ca4c3fca4a9c
Caroline <carolinekimatu6@gmail.com>
2023-01-05 08:45:07 -0800

    02

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 """This is the "add_integer" module.
-It supplies one function, add_integer(a, b=98)"""
+It supplies one function, add_integer(a, b=98)
+a and b must be integers or floats, otherwise raise a TypeError exception
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b"""
 
 
 def add_integer(a, b=98):
     """a function that adds 2 integers and returns the result
-    a and b must be integers or floats, otherwise raise a TypeError exception
-    a and b must be first casted to integers if they are float"""
+    a and b must be integers or floats,
+    otherwise raise a TypeError"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
