6145fc3b8a92799ea8346108e8f738431c94e506
Caroline <carolinekimatu6@gmail.com>
2023-02-14 07:13:29 -0800

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in the database hbtn_0c_0
 -- The database name will be passed as an argument to the mysql command
 -- f the table second_table already exists, your script should not fail
-CREATE TABLE second_table (
+CREATE IF NOT EXISTS TABLE second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
