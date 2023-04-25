38432fa23af6f2a463cc86501c7502f81fe6fe70
Caroline <carolinekimatu6@gmail.com>
2023-04-25 09:09:04 -0700

    task 4-6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+// computes the number of tasks completed by user id
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const tasks = JSON.parse(body);
+  const dict = {};
+  for (let i = 0; i < tasks.length; i++) {
+    if (!dict[tasks[i].userId]) {
+      dict[tasks[i].userId] = 0;
+    }
+    if (tasks[i].completed === true) {
+      dict[tasks[i].userId] += 1;
+    }
+  }
+  console.log(dict);
+});
