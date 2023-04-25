d1a160af9663e610a2c1c3250b32eb26b4285c67
Caroline <carolinekimatu6@gmail.com>
2023-04-25 06:25:25 -0700

    tasks 0-3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// prints the title of a Star Wars movie where
+// the episode number matches a given integer
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
