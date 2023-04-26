c7a633d26205bee66568006a9e709f70f3a221f8
Caroline <carolinekimatu6@gmail.com>
2023-04-25 23:52:14 -0700

    task corrections

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filePath, 'utf-8', content, err => {
+fs.writeFile(filePath, content, err => {
   if (err) {
     console.error(err);
   }
